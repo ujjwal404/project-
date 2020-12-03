5a2c82b73fc14c1d0d8275cc6dc2f73d912fb4b5
ujjwal <ujjwal2022.cs19@nsut.ac.in>
2020-12-03 22:08:12 +0530

    a

---
+++ precache-manifest.fa50b206fa4739f575d5ea87ae96b438.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1dba7952d6956e0d8c9f06bb8c998f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf4f967635a919e0fcd8",
+    "url": "/static/css/main.2619971d.chunk.css"
+  },
+  {
+    "revision": "f89ccdf8e3f4f3d98fff",
+    "url": "/static/js/2.4b064010.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/static/js/2.4b064010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4f967635a919e0fcd8",
+    "url": "/static/js/main.6a8bc7c5.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  },
+  {
+    "revision": "e20e38d040ab5b3b03a509957d440198",
+    "url": "/static/media/home.e20e38d0.svg"
+  }
+]);
